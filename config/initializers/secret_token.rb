160e659680809b0ce3c53b7771a067db89ae061a
saasbook <saasbook@saasbook.(none)>
2014-01-22 00:10:00 +0530

    Created authentication using devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrainStorm2014::Application.config.secret_token = '144f9cfdab7e5c867d1996ebf2e661b68ac30dafdce3c7d27a312a45614d1123e74998ae2f9d1b69a69952b4e140f3a761c2fd7145e93c3dbacd25a49e1881f3'
