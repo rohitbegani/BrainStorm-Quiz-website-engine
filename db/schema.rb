46cab4f04ee4c9dc98ca28a1f0ab4e07e31a972e
saasbook <saasbook@saasbook.(none)>
2014-02-03 20:02:59 +0530

    Minor update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140202053212) do
+ActiveRecord::Schema.define(:version => 20140201085015) do
 
   create_table "levels", :force => true do |t|
     t.string   "question"
